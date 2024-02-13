5e25ba9d09ef82ddb389cc7fe44acdad974c3b6f
Yousef Amar <yousef@amar.io>
2024-02-13 00:09:54 +0000

    add: tweak embedding script to support islamqa-org source

--- src/scripts/create-embeddings.mjs
+++ src/scripts/create-embeddings.mjs
@@ -27,10 +27,47 @@ async function processFile(file, embeddings, collection) {
     return;
   }
 
-  let text = `### Question\n\n${content.question}\n\n\n### Answer\n\n${content.answer}`;
+  let text;
+  
+  if (content.question)
+    text = `### Question\n\n${content.question}\n\n\n### Answer\n\n${content.answer}`;
+  else
+    text = `### Title
+
+${content.title}
+
+### Metadata
+
+Madhab: ${content.madhab ? `[${content.madhab}](${content.madhabURL})` : 'Unknown'}
+Source: ${content.source ? `[${content.source}](${content.sourceURL})` : 'Unknown'}
+Scholars:
+${content.scholars && content.scholars.length > 0 ? content.scholars.map((s, i) => `- [${s}](${content.scholarURLs[i]})`).join('\n') : 'Unknown'}
+
+### Content
+
+${content.content}`;
+
   // truncate to 32k characters
   text = text.substring(0, 9000);
-  const metadata = { id: content.id, url: content.url, question: content.question, answer: content.answer, lang };
+  const metadata = {
+    id: content.id,
+    url: content.url,
+    // lang
+  };
+
+  if (content.question) {
+    metadata.question = content.question;
+    metadata.answer = content.answer;
+  } else {
+    metadata.title = content.title;
+    metadata.madhab = content.madhab;
+    metadata.madhabURL = content.madhabURL;
+    metadata.source = content.source;
+    metadata.sourceURL = content.sourceURL;
+    metadata.scholars = content.scholars;
+    metadata.scholarURLs = content.scholarURLs;
+    metadata.content = content.content;
+  }
 
   console.log(`Processing file: ${file}`);
   await MongoDBAtlasVectorSearch.fromTexts(
@@ -53,7 +90,7 @@ async function main() {
   console.log("Connected to MongoDB");
 
   const embeddings = new OpenAIEmbeddings();
-  const dir = 'data/islamqa-info-' + lang;
+  const dir = 'data/islamqa-org-' + lang;
   const files = fs.readdirSync(dir);
 
   for (const file of files) {
