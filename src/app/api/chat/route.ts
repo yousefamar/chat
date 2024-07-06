24a093772e5454469a89d1d21674e7b78934d402
Yousef Amar <yousef@amar.io>
2024-07-06 19:12:07 +0100

    add: migrate to gemini and some other improvements

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
-import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { ChatVertexAI } from "@langchain/google-vertexai";
 import { formatDocumentsAsString } from "langchain/util/document";
 import { PromptTemplate } from "@langchain/core/prompts";
 import {
@@ -13,6 +14,28 @@ import { MongoClient } from "mongodb";
 import { getDB } from '@/utils/db';
 import { StreamingTextResponse } from 'ai';
 import { StringPromptValue } from 'langchain/prompts';
+import { JsonOutputParser } from '@langchain/core/output_parsers';
+
+const serviceAccount = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'service-account.json'), 'utf-8'));
+
+const safetySettings = [
+  {
+    category: "HARM_CATEGORY_HARASSMENT",
+    threshold: "BLOCK_ONLY_HIGH",
+  },
+  {
+    category: "HARM_CATEGORY_HATE_SPEECH",
+    threshold: "BLOCK_ONLY_HIGH",
+  },
+  {
+    category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    threshold: "BLOCK_ONLY_HIGH",
+  },
+  {
+    category: "HARM_CATEGORY_DANGEROUS_CONTENT",
+    threshold: "BLOCK_ONLY_HIGH",
+  },
+];
 
 export const dynamic = 'force-dynamic';
 export async function POST(request: Request) {
@@ -30,11 +53,20 @@ export async function POST(request: Request) {
 
   const retriever = vectorStore.asRetriever();
 
-  const model = new ChatOpenAI({
-    modelName: "gpt-4-turbo-preview",
-    streaming: true,
-    maxTokens: -1,
-  });
+  // const fastModel = new ChatVertexAI({
+  //   model: "gemini-1.5-flash-001",
+  //   authOptions: { credentials: serviceAccount },
+  //   safetySettings,
+  // }) as any;
+
+  const model = new ChatVertexAI({
+    model: "gemini-1.5-pro-001",
+    // model: "claude-3-5-sonnet@20240620",
+    authOptions: { credentials: serviceAccount },
+    safetySettings,
+  }) as any;
+
+  const fastModel = model;
 
   type QuestionCondenserInput = {
     question: string;
@@ -52,41 +84,64 @@ export async function POST(request: Request) {
       question: (input: QuestionCondenserInput) => input.question,
       history: (input: QuestionCondenserInput) => input.history.map((msg, i) => `${i % 2 === 0 ? 'Human' : 'Assistant'}: ${msg}`).join('\n'),
     },
-    PromptTemplate.fromTemplate(condenseQuestionPrompt),
-    model,
+    async (input: QuestionCondenserInput) => {
+      const sys = await PromptTemplate.fromTemplate(condenseQuestionPrompt).format({
+        history: input.history,
+      });
+
+      return [
+        ['system', sys],
+        ['human', `Chat History:\n\n${input.history}\n\n\nMessage to rephrase:\n\n${input.question}`],
+      ];
+    },
+    fastModel,
     new StringOutputParser(),
   ]);
 
   const translationChain = RunnableSequence.from([
-    {
-      question: new RunnablePassthrough(),
-    },
-    PromptTemplate.fromTemplate(translationPrompt),
-    model,
-    new StringOutputParser(),
-  ]);
-
-  const answerChain = RunnableSequence.from([
-    async (input: { originalQuestion: string, translatedQuestion: string }) => {
-      const docs = await retriever.getRelevantDocuments(`${input.originalQuestion}\n\n${input.translatedQuestion}`);
-      // const context = formatDocumentsAsString(docs);
-      const context = docs.map(doc => `## ${doc.metadata.id}\n\nURL: ${doc.metadata.url}\n\n${doc.pageContent}\n\n---\n`).join('\n'); 
-      // console.log(context);
-      return {
-        context,
-        question: `${input.originalQuestion}`,
-      }
-    },
-    PromptTemplate.fromTemplate(ragPrompt),
-    // (input) => {
-    //   console.log('Answer: ');
+    async (input: string) => [
+      ['system', translationPrompt],
+      ['human', input],
+    ],
+    fastModel,
+    // async (input) => {
+    //   console.log('Translation: ');
     //   console.log(input);
     //   return input;
     // },
-    async (input: StringPromptValue) => [
-      ['system', systemPrompt],
-      ['human', input.value],
-    ],
+    new JsonOutputParser(),
+  ]);
+
+  const answerChain = RunnableSequence.from([
+    async (input: {
+      originalQuestion: string,
+      condensedQuestion: string,
+      translatedCondensedQuestion: string,
+      history: string[],
+    }) => {
+      const docs = await retriever.getRelevantDocuments(`${input.originalQuestion}\n\n${input.translatedCondensedQuestion}`);
+      // const context = formatDocumentsAsString(docs);
+      const context = docs.map(doc => `## ${doc.metadata.id}\n\nURL: ${doc.metadata.url}\n\n${doc.pageContent}\n\n---\n`).join('\n'); 
+
+      // console.log('Answer: ');
+      // console.log(input);
+      // console.log(context);
+
+      const ragContext = await PromptTemplate.fromTemplate(ragPrompt).format({
+        context: context,
+      });
+
+      const _history = history.map((msg: string, i: number) => [ i % 2 === 0 ? 'human' : 'assistant', msg ]);
+
+      // console.log('History: ');
+      // console.log(_history);
+
+      return [
+        ['system', systemPrompt + '\n\n' + ragContext],
+        ..._history,
+        ['human', input.originalQuestion],
+      ];
+    },
     model,
   ]);
 
@@ -101,11 +156,13 @@ export async function POST(request: Request) {
     history
   });
 
-  const translatedQu = await translationChain.invoke(condensedQu);
+  const translatedQuObj = await translationChain.invoke(message) as { translation: string };
 
   const response = await conversationalRetrievalQAChain.stream({
     originalQuestion: message,
-    translatedQuestion: translatedQu,
+    condensedQuestion: condensedQu,
+    translatedCondensedQuestion: translatedQuObj.translation,
+    history,
   });
 
   return new StreamingTextResponse(response);
